16159bc263bdfd39ba47966bde1a97f0c9ea6ae3
Nadeesh Peiris <nash.ucsc@gmail.com>
2022-08-15 07:24:00 +0530

    project initial files

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { randomScrambleForEvent } from "cubing/scramble";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Cubing Test Server');
+});
+
+app.get('/scrable', async (req: Request, res: Response) => {
+    const scramble = await randomScrambleForEvent("333");
+    scramble.log();
+    res.send(scramble.toString());
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
